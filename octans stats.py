77b2506c0c5307bc1c2eacc09755be1d42f9cf95
faint-artist <sparsh.agarwal@ais.amity.edu>
2021-02-05 13:21:44 +0530

    Update octans stats.py

---
+++ octans stats.py
@@ -0,0 +1,9 @@
+# STATISTICS
+
+if query == 'Stats' or 'stats' or 'statistics': # Arithmetic Progression 
+    stats = input("What do you need to find out (mean, median, mode, )>> ")
+
+    if stats == mean:
+        qtype = input("Is the data grouped")
+
+        if qtype == "no":
