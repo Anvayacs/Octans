f2ea590382c9d27e6e66b17fc082480f685db914
faint-artist <sparsh.agarwal@ais.amity.edu>
2021-03-08 00:06:29 +0530

    Create octans light.py

---
+++ octans light.py
@@ -0,0 +1,13 @@
+"""
+ ask for what do you want to find 
+ v u f magnification power
+ v
+ if magnification given?
+ u  
+ if magnification is given ?
+ f 
+ is v and u given?
+ ask what is given for magnification
+ ask if height is given or distance is given
+ """
+
