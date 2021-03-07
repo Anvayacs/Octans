8e136b356010353c6b776fc68715823ec55dcb43
faint-artist <sparsh.agarwal@ais.amity.edu>
2021-03-07 23:19:48 +0530

    nothing realy

---
+++ real numbers.py
@@ -0,0 +1,6 @@
+query = input("Which chapter or formula do you want to solve>> ")
+
+if query == 'real numbers':
+    qtype = input('what do you want to find out>> LCM, HCF, Proof of irrationality, second term when hcf and lcm given')
+    if qtype == 'LCM':
+         
